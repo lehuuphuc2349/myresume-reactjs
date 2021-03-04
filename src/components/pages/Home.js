9020ea4c5676023d90e8b3fa897e57c372aaae8b
lehuuphuc2349 <hphuc23499@gmail.com>
2021-03-04 09:20:40 +0700

    add skill

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,11 +2,13 @@ import React from 'react'
 import '../../App.css';
 import Intro from '../Intro';
 import About from './About';
+import Resume from './Resume';
 function Home() {
     return (
         <>
             <Intro/>            
             <About/>
+            <Resume/>
         </>
     )
 }
