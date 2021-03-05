1fbc648d99ff4e2670549b06c748fccf58303337
lehuuphuc2349 <hphuc23499@gmail.com>
2021-03-05 20:23:31 +0700

    c

---
+++ src/components/Dowload.js
@@ -0,0 +1,14 @@
+// // import Axios from axios;
+// var fileDownload = require("js-file-download");
+// var url = `C:\Users\StarkLee\Desktop\learn\myCV\lehuuphucCV.doc.docx`;
+// var filename = `lehuuphucCV.doc`;
+// function download(url, filename) {
+//   Axios.get(url, {
+//     responseType: "blob",
+//   }).then((res) => {
+//     fileDownload(res.data, filename);
+//     console.log(res);
+//   });
+// }
+// download();
+// export default download;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,67 +1,78 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
-import {Button} from './Button';
-import './Navbar.css'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import "./Navbar.css";
 function Navbar() {
-    const [button, setButton] = useState(true);
-    const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
-    const displayButton = () => {
-        if(window.innerWidth<=960) {
-            setButton(false);
-        }else {
-            setButton(true);
-        }
+  const [button, setButton] = useState(true);
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const displayButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
     }
-    useEffect(() => {
-        displayButton()
-
-    },[])
-    window.addEventListener('resize',displayButton);
-    return (
-        <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to='/' className="navbar-logo" onClick={closeMobileMenu}>
-                        LeHuuPhucCV <i class="fas fa-id-card"></i>
-                    </Link>
-                    <div className="menu-icon" onClick={handleClick}>
-                        <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
-                    </div>
-                    <ul className={click ? "nav-menu active" : "nav-menu"}>
-                        <li className="nav-items">
-                            <Link to='/' className="nav-links" onClick={closeMobileMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className="nav-items">
-                            <Link to='/about' className="nav-links" onClick={closeMobileMenu}>
-                                About
-                            </Link>
-                        </li>
-                        <li className="nav-items">
-                            <Link to='/resume' className="nav-links" onClick={closeMobileMenu}>
-                                Resume
-                            </Link>
-                        </li>
-                        <li className="nav-items">
-                            <Link to='/contact' className="nav-links" onClick={closeMobileMenu}>
-                                Contact 
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='/' className="nav-links-mobile" onClick={closeMobileMenu}>
-                                DOWLOAD CV 
-                            </Link>
-                        </li> 
-                    </ul>
-                   {button&&<Button buttonStyle="btn--outline">DOWLOAD CV</Button>}
-                </div>
-            </nav>    
-            {/* <div className="banner"></div> */}
-        </>
-    )
+  };
+  useEffect(() => {
+    displayButton();
+  }, []);
+  window.addEventListener("resize", displayButton);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            LeHuuPhucCV <i class="fas fa-id-card"></i>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-items">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-items">
+              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                About
+              </Link>
+            </li>
+            <li className="nav-items">
+              <Link
+                to="/resume"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Resume
+              </Link>
+            </li>
+            <li className="nav-items">
+              <Link
+                to="/contact"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                DOWLOAD CV
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">DOWLOAD CV</Button>}
+        </div>
+      </nav>
+      {/* <div className="banner"></div> */}
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
